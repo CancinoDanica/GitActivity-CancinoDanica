1d699bbe5ebd739683fc2c02ce57ce4f24e60e22
CancinoDanica <danicacancino28@gmail.com>
2024-02-20 10:50:11 +0800

    My Personal Info added

---
+++ git-python-Cancino.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 20 10:15:34 2024
+
+@author: Cancino Danica
+"""
+
+print("fullname: Cancino Danica")
+print("address: Cawit,Boac Marinduque")
+print("birthday: September 1,2002")
